db65d5fbd18571d953cc7a83f922013b65ea8b66
Milena Rocha <67220184+milenasrocha@users.noreply.github.com>
2023-08-15 18:20:38 -0300

    Added Singleton T

---
+++ Interview Task/Assets/_Scripts/Utilities/Singleton.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace LittleSimWorld
+{
+    public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        public static T Instance { get; private set; }
+
+        protected void Awake()
+        {
+            if (Instance != null)
+            {
+                Debug.LogWarning($"Destroying an instance of Singleton because another instance was found. " +
+                                                $"The instances are {gameObject.name} and {Instance.gameObject.name}. " +
+                                                $"Destroying {gameObject.name}.");
+
+                Destroy(gameObject);
+            }
+
+            Instance = this as T;
+        }
+
+        protected virtual void OnApplicationQuit()
+        {
+            Instance = null;
+            Destroy(gameObject);
+        }
+    }
+}
